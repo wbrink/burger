17bd72c609d4405feb2fefd52a70e4fe2ddce29f
wbrink <will.brink55@gmail.com>
2020-03-27 23:54:55 -0700

    setup server.js and example route for burger controller

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// middleware to parse request body data
+app.use(express.urlencoded({extended: false})); // can't send nested object
+app.use(express.json());
+
+//setup express handlebars
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// use router
+const burgerRouter = require("./controllers/burgersController");
+app.use(burgerRouter);
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on localhost:${PORT}`);
+});
